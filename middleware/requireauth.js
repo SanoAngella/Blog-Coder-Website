51072340a358dc2fd7e5c3ebc27503f9e8f3dca1
SanoAngella <sanoangella9@gmail.com>
2026-04-23 19:36:02 +0200

    Add login page

---
+++ middleware/requireauth.js
@@ -0,0 +1,9 @@
+function requireAuth(req, res, next) {
+  if (req.session && req.session.isAuthenticated) {
+    return next();
+  }
+
+  return res.redirect('/login');
+}
+
+module.exports = requireAuth;
